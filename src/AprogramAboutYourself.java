351644f72338df49db5f561e93eead4c7cb3f2b7
gtassiss <gtassiss@montgomerycollege.edu>
2017-10-11 15:25:46 -0400

    Initial commit

---
+++ src/AprogramAboutYourself.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+
+public class AprogramAboutYourself {
+
+public static void main(String[] args) {
+    // what this code does is print my name and age alon and nothing lese 
+    System.out.println("My Name is Game I am 27 years old");
+
+}
+}
